50f6451b5ce37839fff48fdd74ee2690797e57c0
RedGuy12 <paul@reid-family.org>
2023-05-25 21:15:58 -0500

    work on new
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- assets/slideshow.js
+++ assets/slideshow.js
@@ -1,1 +1,20 @@
-new Splide(".splide",{rewind:true,fixedHeight:"500px"}).mount();
+new Splide(".splide", {
+	rewind: true,
+	speed: 750,
+	rewindSpeed: 500,
+	rewindByDrag: true,
+	fixedHeight: "500px",
+	autoplay: true,
+	interval: 7500,
+	lazyLoad: "nearby",
+	breakpoints: {
+		640: { destroy: true },
+	},
+	start: 2
+}).mount();
+
+// todo:
+// speed up arrow transitions
+// center arrows
+// move pagination down
+// hide arrows on not hover
