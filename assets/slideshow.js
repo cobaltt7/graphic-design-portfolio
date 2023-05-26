9afffe17e6aac25bca8f7bcb8ee0101c30da5f82
RedGuy12 <paul@reid-family.org>
2023-05-25 21:27:48 -0500

    lint-y sutffs
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- assets/slideshow.js
+++ assets/slideshow.js
@@ -1,20 +1,23 @@
+/* eslint-disable */
 new Splide(".splide", {
-	rewind: true,
-	speed: 750,
-	rewindSpeed: 500,
-	rewindByDrag: true,
-	fixedHeight: "500px",
 	autoplay: true,
-	interval: 7500,
-	lazyLoad: "nearby",
 	breakpoints: {
 		640: { destroy: true },
 	},
-	start: 2
+	fixedHeight: "500px",
+	interval: 7500,
+	lazyLoad: "nearby",
+	rewind: true,
+	rewindByDrag: true,
+	rewindSpeed: 500,
+	speed: 750,
+	start: 2,
 }).mount();
 
-// todo:
-// speed up arrow transitions
-// center arrows
-// move pagination down
-// hide arrows on not hover
+/*
+ * Todo:
+ * speed up arrow transitions
+ * center arrows
+ * move pagination down
+ * hide arrows on not hover
+ */
