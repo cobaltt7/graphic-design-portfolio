b5c013afc6f1958869845599a2aaf328f838274f
cobaltt7 <cobaltt7@users.noreply.github.com>
2024-11-29 17:25:33 +0000

    Format

---
+++ node_modules/css-prop-sort/node_modules/eslint-plugin-putout/lib/wrap.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const prepare = (plugin, context, options) => (node) => {
+    const {filter, report} = plugin;
+    
+    const source = context.getSourceCode();
+    const getText = source.getText.bind(source);
+    
+    const text = getText(node);
+    
+    const result = filter({
+        text,
+        node,
+        options,
+        getText,
+    });
+    
+    if (!result)
+        return;
+    
+    const fix = prepareFix(plugin.fix, {
+        node,
+        text,
+        getText,
+    });
+    
+    context.report({
+        node,
+        message: report(node),
+        fix,
+    });
+};
+
+const prepareFix = (fix, {node, text, getText}) => (fixer) => {
+    const fixed = fix({
+        node,
+        text,
+        getText,
+    });
+    
+    return [
+        fixer.replaceText(node, fixed),
+    ];
+};
+
+module.exports = (plugin) => {
+    const meta = getMeta(plugin);
+    
+    return {
+        meta,
+        create(context) {
+            const {options} = context;
+            const prepared = prepare(plugin, context, options);
+            const names = plugin.include({options});
+            
+            return getTraversers(names, prepared);
+        },
+    };
+};
+
+function getMeta(plugin) {
+    const {
+        type = 'layout',
+        category = 'spacing',
+        recommended = true,
+        fixable = 'whitespace',
+    } = plugin;
+    
+    return {
+        type,
+        docs: {
+            category,
+            recommended,
+        },
+        fixable,
+    };
+}
+
+function getTraversers(names, plugin) {
+    const traversers = {};
+    
+    for (const name of names)
+        traversers[name] = plugin;
+    
+    return traversers;
+}
+
